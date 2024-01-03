f06ba81675261b070a0ac66178c2e6daffb53cc5
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-01-04 00:31:21 +0200

    UPD: index.css

--- src/components/Title/Title.styled.ts
+++ src/components/Title/Title.styled.ts
@@ -5,10 +5,8 @@ const TitleStyled = styled.h2`
     color: ${({ theme }) => (theme as any).palette?.text.primary};
     text-align: center;
     font-family: 'Manrope Medium';
-    font-size: 36px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-    letter-spacing: 1.44px;
+    font-size: 2.25rem;
+    line-height: 1.36;
+    letter-spacing: 0.09rem;
 `
 export default TitleStyled
