731541a3ebdd211df8c97d1a22eb9cab34cd77d4
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-01-07 20:52:56 +0200

    UPD: LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,4 @@
-import { useLocation, useNavigate } from 'react-router-dom'
-
 function LoginPage() {
-    const navigate = useNavigate()
-    const location = useLocation()
-
-    const fromPage: string = location.state?.from?.pathname || '/'
-    console.log('fromPage:', fromPage)
-
     return <div>LoginPage</div>
 }
 
